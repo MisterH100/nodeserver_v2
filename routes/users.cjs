e1ff1d91e52926ef129dc3db6eb152e27a096f91
misterh100 <handsomenyathi1@gmail.com>
2023-10-22 19:31:19 +0200

    add get and put route for users

--- routes/users.cjs
+++ routes/users.cjs
@@ -5,16 +5,15 @@ const jwt = require('jsonwebtoken');
 const { generateFromEmail, generateUsername } = require("unique-username-generator");
 const ROUNDS = 10;
 
-
+//register
 router.post("/register", async (req, res) => {
+    const newName = req.body.name;
+    const newSurname = req.body.surname
+    const newEmail =  req.body.email;
+    const newPassword = req.body.password;
+    const newUsername = generateFromEmail(newEmail);
 
     try{
-        const newName = req.body.name;
-        const newSurname = req.body.surname
-        const newEmail =  req.body.email;
-        const newPassword = req.body.password;
-        const newUsername = generateFromEmail(newEmail);
-
         await User.findOne({email: newEmail})
         .then((user) =>{
 
@@ -46,38 +45,41 @@ router.post("/register", async (req, res) => {
 })
 
 
+//login
 router.post("/login", async(req,res) =>{
     const usernameLogIn = req.body.username;
     const passwordLogIn = req.body.password;
     try {
        await User.findOne({username: usernameLogIn})
        .then(user =>{
-        if(!user){
-            res.status(404).json("user does not exist");
-        }
-
-        if(user){
-            const validatePassword = bcrypt.compareSync(passwordLogIn, user.password);
-            const tokenId = user._id;
-
-            if(!validatePassword){
-                res.status(404).json("wrong credentials");;
+            if(!user){
+                res.status(404).json("user does not exist");
             }
-            if(validatePassword){
-                const {password, ...details} = user._doc;
-                const token = jwt.sign({ tokenId}, process.env.JWT_SECRET,{ expiresIn: '1d' });
-                res.status(200).json({user:details,token:token});
+
+            if(user){
+                const validatePassword = bcrypt.compareSync(passwordLogIn, user.password);
+                const tokenId = user._id;
+
+                if(!validatePassword){
+                    res.status(404).json("wrong credentials");;
+                }
+                if(validatePassword){
+                    const {password, ...details} = user._doc;
+                    const token = jwt.sign({ tokenId}, process.env.JWT_SECRET,{ expiresIn: '1d' });
+                    res.status(200).json({user:details,token:token});
+                }
             }
-        }
-       })
+        })
     } catch (error) {
         res.send(error)
     }
 })
 
 
+//auth
 const verifyAuth = (req,res,next) =>{
     const token = req.headers["auth-token"];
+    
     if(!token){
         res.json({authenticated: false})
     }
@@ -114,6 +116,45 @@ router.post("/auth", verifyAuth, async(req,res) =>{
     }
 })
 
+//get user
+router.get("/user/:id", async(req,res) =>{
+    const userId = req.params.id;
+
+    try {
+        await User.findById(userId)
+        .then(user=>{
+            res.send(user)
+        })
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+//update username
+router.put("/user/update/:id", async(req,res) =>{
+    const userId = req.params.id;
+    const newUsername = req.body.username;
+    try {    
+        await User.findOne({username: newUsername})
+        .then(user =>{
+            if(!user){ 
+                try {
+                    User.updateOne({"_id": userId}, {$set:{"username": newUsername}})
+                    .then(()=>{
+                        res.json("username updated")
+                    })
+                } catch (error) {
+                    res.json(error)
+                }
+            }
+            if(user){
+                res.json("this username is taken")
+            }
+        })
+    } catch (error) {
+        res.json(error)
+    }
+})
 
 module.exports = router;
 
