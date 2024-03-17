f0e0355e2175f09950719e8b900a46248e7fe4b0
misterh100 <handsomenyathi1@gmail.com>
2024-03-17 14:08:08 +0200

    add ability to send emails on order

--- models/product_orders.cjs
+++ models/product_orders.cjs
@@ -1,30 +1,44 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 //Data schema
-const productOrdersSchema = new mongoose.Schema({
-  email: {
-    type: String,
+const productOrdersSchema = new mongoose.Schema(
+  {
+    order_number: {
+      type: String,
+    },
+    first_name: {
+      type: String,
+    },
+    last_name: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: Number,
+    },
+    address: {
+      type: String,
+    },
+    products: [{ type: Schema.Types.ObjectId, ref: "products" }],
+    payment_method: {
+      type: String,
+    },
+    terms: {
+      type: Boolean,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    orderDate: {
+      type: Date,
+      default: Date.now(),
+    },
   },
-  name: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-  },
-  phone: {
-    type: Number,
-  },
-  products: {
-    type: [],
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-  orderedAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("product_orders", productOrdersSchema);
--- routes/product_orders.cjs
+++ routes/product_orders.cjs
@@ -1,23 +1,114 @@
 const router = require("express").Router();
+const orderid = require("order-id")("key");
+const sgMail = require("@sendgrid/mail");
+require("dotenv").config();
 const ProductOrder = require("../models/product_orders.cjs");
 
 //Submit
 
 router.post("/products/orders", async (req, res) => {
-    try {
-        const newProductOrder = new ProductOrder({
-            email: req.body.email,
-            name: req.body.name,
-            lastName: req.body.lastName,
-            phone: req.body.phone,
-            products: req.body.products
-        });
-        const order = await newProductOrder.save();
-        res.send(order);
-    }
-    catch (err) {
-        res.send(err);
-    }
-})
+  const orderNumber = orderid.generate();
+  try {
+    const newProductOrder = new ProductOrder({
+      order_number: orderNumber,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      phone: req.body.phone,
+      address: req.body.address,
+      products: req.body.products,
+      payment_method: req.body.payment_method,
+      terms: req.body.terms,
+    });
+    const order = await newProductOrder.save();
+    const { order_number, payment_method, address, ...other } = order._doc;
+    res.send({
+      order_number: order_number,
+      payment_method: payment_method,
+      address: address,
+    });
+
+    //send message to client
+    sgMail.setApiKey(process.env.EMAIL_API_KEY);
+    const msg = {
+      to: req.body.email,
+      from: process.env.FROM_EMAIL,
+      subject: "Order Confirmation",
+      text:
+        "ProductStore Hi " +
+        req.body.first_name +
+        " Your order: " +
+        orderNumber +
+        " has been received and processed, our agent will be in touch with you shortly",
+      html: `<h1>Order received</h1><br/>
+                <h3>ProductStore Official</h3>
+                <p>Order Number: ${orderNumber}</p>
+                <p>Your order has been received and processed, our agent will be in touch with you shortly</p>
+                `,
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log("Email sent client");
+      })
+      .catch((error) => {
+        console.error(error);
+        console.log("Email not sent");
+      });
+
+    //notify admin
+    const notification = {
+      to: process.env.TO_EMAIL,
+      from: process.env.FROM_EMAIL,
+      subject: "New order",
+      text:
+        "ProductStore A new order has been placed with order number: " +
+        orderNumber,
+      html: `<h1>New order</h1> <br/> 
+            <h3>ProductStore Official</h3>
+            <p> A new order has been placed with order number: ${orderNumber}</p> <br/>`,
+    };
+    sgMail
+      .send(notification)
+      .then(() => {
+        console.log("Email sent to admin");
+      })
+      .catch((error) => {
+        console.error(error);
+        console.log("Email not sent");
+      });
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// get order products
+router.get("/order_products/:order_number", (req, res) => {
+  const orderNumber = req.params.order_number;
+  try {
+    ProductOrder.findOne({ order_number: orderNumber })
+      .populate("products")
+      .then((order) => {
+        const { products, ...other } = order._doc;
+        res.send({ products: products });
+      });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+//get orders
+router.get("/orders", (req, res) => {
+  try {
+    ProductOrder.find()
+      .populate("products")
+      .sort({ createdAt: "descending" })
+      .then((orders) => {
+        res.send(orders);
+      });
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 module.exports = router;
