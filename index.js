949c5044d92f8a85902620215b94303c19bf8521
misterh100 <handsomenyathi1@gmail.com>
2024-03-23 19:15:47 +0200

    add array og origins

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ const app = express();
 dotenv.config();
 app.use(
   cors({
-    origin: process.env.ORIGINS.split(","),
+    origin: [
+      "http://localhost:3000",
+      "https://productshop-official.vercel.app",
+    ],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
