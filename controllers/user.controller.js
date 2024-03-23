fa0cfbaad5f3fa7c87e652d996e906663862dbda
misterh100 <handsomenyathi1@gmail.com>
2024-03-23 19:36:46 +0200

    not register user if there are conflits in emails

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,38 +9,39 @@ export const RegisterUser = async (req, res) => {
   try {
     const user = await User.findOne({ email: email });
     if (user) {
-      res.status(400).json({ message: "this email is taken" });
-    }
-    const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(password, salt);
-    const maleProfilePic = `https://avatar.iran.liara.run/public/boy?username=${first_name}`;
-    const femaleProfilePic = `https://avatar.iran.liara.run/public/girl?username=${first_name}`;
-    const newUser = new User({
-      first_name: first_name,
-      last_name: last_name,
-      email: email,
-      password: hashedPassword,
-      phone: phone,
-      gender: gender,
-      address: address,
-      profileImage: gender === "male" ? maleProfilePic : femaleProfilePic,
-    });
+      res.json({ message: "this email is taken" });
+    } else {
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      const maleProfilePic = `https://avatar.iran.liara.run/public/boy?username=${first_name}`;
+      const femaleProfilePic = `https://avatar.iran.liara.run/public/girl?username=${first_name}`;
+      const newUser = new User({
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        password: hashedPassword,
+        phone: phone,
+        gender: gender,
+        address: address,
+        profileImage: gender === "male" ? maleProfilePic : femaleProfilePic,
+      });
 
-    setJWTCookie(newUser._id, res);
-    await newUser.save();
-    res.status(200).json({
-      message: "user registered successfully",
-      user: {
-        _id: newUser._id,
-        first_name: newUser.first_name,
-        last_name: newUser.last_name,
-        email: newUser.email,
-        phone: newUser.phone,
-        gender: newUser.gender,
-        address: newUser.address,
-        profileImage: newUser.profileImage,
-      },
-    });
+      setJWTCookie(newUser._id, res);
+      await newUser.save();
+      res.status(200).json({
+        message: "user registered successfully",
+        user: {
+          _id: newUser._id,
+          first_name: newUser.first_name,
+          last_name: newUser.last_name,
+          email: newUser.email,
+          phone: newUser.phone,
+          gender: newUser.gender,
+          address: newUser.address,
+          profileImage: newUser.profileImage,
+        },
+      });
+    }
   } catch (err) {
     res.send(err);
   }
@@ -51,13 +52,13 @@ export const LoginUser = async (req, res) => {
   try {
     const user = await User.findOne({ email: email });
     if (!user) {
-      res.status(404).json({ message: "user does not exist" });
+      res.json({ message: "user does not exist" });
     }
 
     const validatePassword = bcrypt.compareSync(password, user.password);
 
     if (!validatePassword) {
-      res.status(404).json({ message: "wrong credentials" });
+      res.status(400).json({ message: "wrong credentials" });
     }
     if (validatePassword) {
       setJWTCookie(user._id, res);
@@ -89,7 +90,7 @@ export const authUser = async (req, res) => {
     const user = await User.findById(userID);
 
     if (!user) {
-      res.status(400).json({ message: "user does not exist" });
+      res.json({ message: "user does not exist" });
     }
 
     const { password, ...details } = user._doc;
