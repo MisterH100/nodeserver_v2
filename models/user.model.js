296de6512fb545086d055d7bb8371e9b4d49c16a
misterh100 <handsomenyathi1@gmail.com>
2024-03-24 16:08:59 +0200

    add verified fie to users

--- models/user.model.js
+++ models/user.model.js
@@ -31,6 +31,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
     profileImage: {
       type: String,
       default: "",
