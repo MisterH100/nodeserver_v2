8e3c4e56b5781756efee0a189f8962880f1d2734
misterh100 <handsomenyathi1@gmail.com>
2024-03-26 11:20:37 +0200

    configure cors

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectToDatabase from "./db/connectToDatabase.js";
 import productRoute from "./routes/product.route.js";
@@ -16,13 +15,7 @@ const origins = process.env.ORIGINS.split(",");
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: origins,
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use("/", homeRoute);
 app.use("/api", productRoute);
 app.use("/api", productOrderRoute);
